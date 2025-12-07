32fe42847e33d53fa7a42bfc7a64fe1e1665d485
Mijura <miodragvilotijevic@gmail.com>
2025-12-07 14:34:23 +0100

    planning agent example

---
+++ workflow/planning.ts
@@ -0,0 +1,10 @@
+import { Command, PlanningAgent, Workflow } from "@jigjoy-io/mosaic"
+import 'dotenv/config'
+
+const command: Command = {
+    model: 'gpt-5'
+}
+const planner = new PlanningAgent(command)
+const workflow: Workflow = await planner.planFromGoal('Implement login functionality ')
+console.log(workflow)
+// await workflow.execute()
